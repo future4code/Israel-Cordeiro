6f32b43fe64f1f5c206d71646fb8eb1d52963095
israel-cordeiro <israelcordeiro@hotmail.com>
2021-06-19 17:37:47 -0300

    final projeto

--- semana1/lista-logica/lista-exercicios-intro-js/lista-exercicios-intro-js/exercicios.js
+++ semana1/lista-logica/lista-exercicios-intro-js/lista-exercicios-intro-js/exercicios.js
@@ -104,31 +104,55 @@ function retornaUltimoElemento(array) {
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
-  const trocaArray = array[0]
+  const primeiraPosicao = array[0]
+  const ultimaPosicao = array[array.length-1]
+  array[0] = ultimaPosicao
+  array[array.length-1]= primeiraPosicao
   
+  return array
 
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
-  // implemente sua lógica aqui
+   string1 = string1.toUpperCase()
+   string2 = string2.toUpperCase()
+
+   return string1 === string2
 
 }
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
-  // implemente sua lógica aqui
+  const anoAtual = Number (prompt('Em que ano estamos?'))
+  const anoNascimento = Number (prompt('Qual seu ano de Nascimento?'))
+  const anoCateiraIdentida = Number (prompt('Qual ano da sua carteira de identidade?'))
+  const idade = anoAtual - anoNascimento
+  const emissaoRg = anoAtual - anoCateiraIdentida
+  const primeiraCondicao = (idade <= 20 && emissaoRg >=5)
+  const segundaCondicao = (idade > 20 && idade <= 50 && emissaoRg >= 10)
+  const terceiraCondicao = (idade > 50 && emissaoRg >= 15)
+  const resultado = (primeiraCondicao || segundaCondicao || terceiraCondicao)
 
+  console.log(resultado)
 }
 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
-  // implemente sua lógica aqui
 
+  const primeiraCondicao = ano % 400 === 0
+  const segundaCondicao = ano % 4 === 0
+  const terceiraCondicao = ano % 100 === 0
+  const resultado = primeiraCondicao || segundaCondicao && !terceiraCondicao
+  return resultado
 }
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
-  // implemente sua lógica aqui
+  const idade = prompt ('Você tem mais de 18 anos? responda Sim ou Nao')
+  const ensinoMedio = prompt ('Você possui ensino médio completo? resonda Sim ou Nao')
+  const disponibilidadeDoCurso = prompt ('Você possui disponibilidade exclusiva durante os horários do curso? responda Sim ou Nao')
+  const resultado = idade === 'sim' && ensinoMedio === 'sim' && disponibilidadeDoCurso === 'sim'
+  console.log (resultado)
 
 }
