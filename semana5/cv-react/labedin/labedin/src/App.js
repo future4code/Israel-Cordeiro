e5da8f5e4bca633ace980902b99cb180a71ddae2
israel-cordeiro <israelcordeiro@hotmail.com>
2021-07-13 21:30:00 -0300

    atualização final

--- semana5/cv-react/labedin/labedin/src/App.js
+++ semana5/cv-react/labedin/labedin/src/App.js
@@ -1,65 +1,95 @@
 import React from 'react';
-import './App.css';
 import CardGrande from './components/CardGrande/CardGrande';
 import CardPequeno from './components/CardPequeno/CardPequeno';
 import ImagemButton from './components/ImagemButton/ImagemButton';
 import israel from "./img/perfil.jpg"
+import email from "./img/icone-email.png"
+import pin from "./img/pin-de-localizacao.png"
+import styled from "styled-components"
+
+const Mae = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 30px;
+
+`
+const Container = styled.div`
+  width: 40vw;
+  margin: 10px 0;
+
+` 
+
+const H2 = styled.h2`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+
+` 
+
 
 
 function App() {
   return (
-    <div className="App">
-      <div className="page-section-container">
-        <h2>Dados pessoais</h2>
-        <CardGrande 
-          imagem= {israel}
-          nome="Israel" 
-          descricao="Oi, eu sou o Israel. Sou estudante do Curso Web Full Stack na Labenu. Iniciei minha jornada em vendas no ramo do varejo e trabalhei em algumas empresas como Hering, Kipling e Sephora. Agora iciando no ramo da Programação."
-        />
-        
-        <ImagemButton 
-          imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
-          texto="Ver mais"
-        />
-      </div>
+      <Mae>
+        <Container>
+          <H2>Dados pessoais</H2>
+          <CardGrande 
+            imagem= {israel}
+            nome="Israel" 
+            descricao="Oi, eu sou o Israel. Sou estudante do Curso Web Full Stack na Labenu. Iniciei minha jornada em vendas no ramo do varejo e trabalhei em algumas empresas como Hering, Kipling e Sephora. Agora iciando no ramo da Programação."
+          />
+          
+          <ImagemButton 
+            imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
+            texto="Ver mais"
+          />
+        </Container>
 
-      <div className="endereco-email-container">
-        <h2> Endereço de E-mail</h2>
-        <CardPequeno
-          endereco ="israelcordeiro@hotmail.com"
-        />
+        <Container>
+          <CardPequeno
+            endereco ="israelcordeiro@hotmail.com"
+            imagem= {email}
+            texto= "E-mail:"
+          />
+        </Container>
 
+        <Container>
+          <CardPequeno
+            endereco ="Av João Gerosa nº 90 Centro- Serra Negra- SP"
+            imagem= {pin}
+            texto= "Endereço:"
+          />
+        </Container>
 
-      </div>
+        <Container>
+          <H2>Experiências profissionais</H2>
+          <CardGrande 
+            imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+            nome="Labenu" 
+            descricao="Formando desenvolvedores para o mercado de trabalho!" 
+          />
+          
+          <CardGrande 
+            imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
+            nome="NASA" 
+            descricao="Apontando defeitos." 
+          />
+        </Container>
 
-      <div className="page-section-container">
-        <h2>Experiências profissionais</h2>
-        <CardGrande 
-          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
-          nome="Labenu" 
-          descricao="Formando desenvolvedores para o mercado de trabalho!" 
-        />
-        
-        <CardGrande 
-          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
-          nome="NASA" 
-          descricao="Apontando defeitos." 
-        />
-      </div>
+        <Container>
+          <H2>Minhas redes sociais</H2>
+          <ImagemButton 
+            imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+            texto="Facebook" 
+          />        
 
-      <div className="page-section-container">
-        <h2>Minhas redes sociais</h2>
-        <ImagemButton 
-          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
-          texto="Facebook" 
-        />        
-
-        <ImagemButton 
-          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
-          texto="Twitter" 
-        />        
-      </div>
-    </div>
+          <ImagemButton 
+            imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+            texto="Twitter" 
+          />        
+        </Container>
+      </Mae>
   );
 }
 
--- semana5/cv-react/labedin/labedin/src/components/CardPequeno/CardPequeno.js
+++ semana5/cv-react/labedin/labedin/src/components/CardPequeno/CardPequeno.js
@@ -1,13 +1,36 @@
 import React from 'react';
 
+import styled, { css } from "styled-components"
+
+const Div = styled.div `
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    padding: 20px 10px;
+    margin-bottom: 10px;
+    height: 75px;
+`
+
+const Imagem = styled.img `
+    width: 38px;
+    margin-right: 10px;
+    border-radius: 50%;
+
+`
+
+const Texto = styled.p `
+    font-weight: bold;
+    margin-right: 3px;
+`
+
+
 function CardPequeno(props) {
     return (
-        <div className ="endereco-email-container" >
+        <Div >
+        <Imagem  src= {props.imagem} />
+        <Texto> {props.texto}</Texto>
         <p>{props.endereco} </p>
-        </div>
-
-
-
+        </Div>
 
     )
 }
