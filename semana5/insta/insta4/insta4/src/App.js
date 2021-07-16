7b31ef13ac3e86b235b4e0b4fd0827ab238cb470
israel-cordeiro <israelcordeiro@hotmail.com>
2021-07-15 21:24:08 -0300

    atualização

--- semana5/insta/insta4/insta4/src/App.js
+++ semana5/insta/insta4/insta4/src/App.js
@@ -1,45 +1,86 @@
 import React from 'react';
-import styled from 'styled-components'
-import Post from './components/Post/Post';
+//import styled from 'styled-components'
+//import Post from './components/Post/Post';
 import Imagem from "./imagem/flor.jpg"
 import Perfil from "./imagem/perfil.jpg"
 import Feed from "./imagem/leao.jpeg"
 
-const MainContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-`
-
-class App extends React.Component {
-  render() {
-    return (
-      <MainContainer>
-        
-        <Post
-          nomeUsuario={'paulinhaa'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-
-        <Post
-          nomeUsuario={'Israel'}
-          fotoUsuario={Perfil}
-          fotoPost={Feed}
-        />
-
-        <Post
-          nomeUsuario={'Thais'}
-          fotoUsuario={Imagem}
-          fotoPost={Imagem}
-        />
 
 
+  class App extends React.Component {
+    state = {
+      posts: [
+        {
+          nomeUsuario: 'Paulinha',
+          fotoUsuario: 'https://picsum.photos/50/50',
+          fotoPost: 'https://picsum.photos/200/150'
+        },
+        {
+          nomeUsuario: 'Israel',
+          fotoUsuario: {Perfil},
+          fotoPost: {Feed}
+        },
+        {
+          nomeUsuario:'Thais',
+          fotoUsuario:{Imagem},
+          fotoPost: {Imagem}
+        }
+      ],
 
-      </MainContainer>
-    );
-  }
+      valorInputNome: '',
+      valorInputFotoUsuario: '',
+      valorInputFotoPost: ''
+
+    }
+    render() {
+
+      const listaDePost = this.state.post.map ((post) => {
+        return(
+          <post
+          {...post.nomeUsuario}
+          {...post.fotoUsuario}
+          {...post.fotoPost}
+          />
+        )
+    })
+
+
+    onChangeInputNome = (event) =>{
+      this.setState ({valorInputNome: event.target.value})
+    }
+
+    onChangeInputFotoUsuario = (event) =>{
+      this.setState ({valorInputFotoUsuario: event.target.value})
+    }
+
+    onChangeInputFotoPost = (event) =>{
+      this.setState ({valorInputFotoPost: event.target.value})
+    }
+
+      return (
+        <div>
+           {listaDePost}
+            
+            <input
+            value={this.state.valorInputNome}
+            onChange={this.onChangeInputNome}
+            placeholder= 'nome'
+            />
+
+            <input
+            value={this.state.valorInputFotoUsuario}
+            onChange={this.onChangeInputFotoUsuario}
+            placeholder= 'Foto do Usuario'
+            />
+
+            <input
+            value={this.state.valorInputFotoPost}
+            onChange={this.onChangeInputFotoPost}
+            placeholder='Foto do Post'
+            />
+        </div>
+          
+      )
+   }
 }
-
-export default App;
+  export default App;
