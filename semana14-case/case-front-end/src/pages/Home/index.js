af96f9030c59ca6f642e5cb679feb8054d13a5b0
israel-cordeiro <israelcordeiro@hotmail.com>
2021-09-17 19:39:00 -0300

    atualizacao

--- semana14-case/case-front-end/src/pages/Home/index.js
+++ semana14-case/case-front-end/src/pages/Home/index.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
-import { ContainerCard, ContainerBackAndFrontCard } from './styles';
+import { ContainerCard, CardContainer} from './styles';
 
 
 const Home = () =>{
   
     const [cards, setCards] = useState([])
-    const  [virar, setVirar] = useState([])
+    const  [turn, setTurn] = useState([])
     const [flipCard, setFlipCard] = useState(false)
+  
 
     console.log(cards)
     
@@ -17,7 +18,7 @@ const Home = () =>{
 
         .then((res) => {
             setCards(res.data.cards)
-            setVirar({url: res.data.imagesUrl, cardsBack: res.data.imageBackCard})
+            setTurn({url: res.data.imagesUrl, cardsBack: res.data.imageBackCard})
         })
         .catch((err) => {
           console.log(err)
@@ -25,20 +26,18 @@ const Home = () =>{
     })
 
    
-    const tarotCartas = cards && cards.map((itens) =>{
+    const tarotCards = cards && cards.map((itens) =>{
         return(
             <div key={itens.cards}>
-                <h4>{itens.name}</h4>
-                  <img src={`${virar.url}${itens.image}`} />
+                  <img src={`${turn.url}${itens.image}`} />
             </div>
         )
     })
 
     const renderCardsBack = cards && cards.map((itens) =>{
         return(
-            <div key={itens.cards}>
-                <h4>{itens.name}</h4>
-                <img src={`${virar.cardsBack}`} />
+            <div key={itens.cards} onClick={() => willSelectACard(itens.name)}> 
+                <img src={`${turn.cardsBack}`} />
             </div>
         )
     })
@@ -47,68 +46,29 @@ const Home = () =>{
         listaCartas()
     }, [])
 
+     const array = cards;
+
     const turnCard = () =>{
-        setFlipCard(true)
+        setFlipCard(!flipCard)
+        array.sort((a,b) => 0.5 - Math.random())
     }
 
-
+    const willSelectACard = (name) =>{
+        setFlipCard(!name)
+        alert(name)
+    }
+    
+    
     return(
-        <div>
-            <h1>Escolha uma Carta</h1>
-            <button onClick={()=>turnCard()}>Click To Flip</button>
-            <div>
-            {flipCard ? tarotCartas: renderCardsBack}
-            </div>
-            
-        </div>
+        <ContainerCard>
+            <h1>Jogo de Tarot</h1>
+            <button onClick={()=>turnCard()}>Iniciar Jogo</button>
+            <CardContainer>
+            {flipCard ? renderCardsBack: tarotCards}
+            </CardContainer>  
+        </ContainerCard>
     )
 }
 
 export default Home; 
-
-  {/* <h4>{itens.name}</h4> */}
-
-
-//   const [cartas, setCartas] = useState([])
-
-//   console.log(cartas)
-  
-//   const listaCartas = (() => {
-
-//       axios.get('tarot.json')
-
-//       .then((res) => {
-//           console.log(res.data)
-//           setCartas(res.data.cards)
-//       })
-//       .catch((err) => {
-//         setCartas(err.data)
-//       }) 
-//   })
-
-//   useEffect(() =>{
-//       listaCartas()
-//   }, [])
-
-//   const imagesUrl = "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/162x341/"
-
-//   const imageBackCard = "https://dkw5ssdvaqf8l.cloudfront.net/static/psr/br/framework/yii/images/content/pt-br/product/tarot/marselha/back-blue-card.png"
-
-//   const tarotCartas = cartas && cartas.map((itens) =>{
-//       return(
-//           <ContainerCard key={itens.cards}>
-//               <ContainerBackAndFrontCard>
-//                 <img src={`${imagesUrl}${itens.image}`} />
-//                  <img src={imageBackCard} />
-//               </ContainerBackAndFrontCard>
-//           </ContainerCard>
-//       )
-//   })
-//   return(
-//       <div>
-//           <h1>Escolha uma Carta</h1>
-//           {tarotCartas}
-//       </div>
-//   )
-// }
 
---
+++ semana14-case/case-front-end/src/pages/Home/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const ContainerCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+
+    button{
+        width: 100px;
+    }
+   
+` 
+export const CardContainer = styled.div`
+     display: grid;
+     margin: 20px;
+     grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+   
+  
+
+`
